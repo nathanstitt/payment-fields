843733ad043795b35859994ee1a1d0404454075e
Nathan Stitt <nathan@stitt.org>
2019-03-01 22:27:29 -0600

    set version

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,12 @@
 module.exports = {
     "extends": "argosity",
     "parser": "babel-eslint",
+    "settings": {
+        "react": {
+            "pragma": "React", // Pragma to use, default to "React"
+            "version": "16.0", // React version, default to the latest React stable release
+        },
+    },
     rules: {
         'no-underscore-dangle': 0,
     },
